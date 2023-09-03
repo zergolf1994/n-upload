953165d3a2e773871160d1d5f4d874f09c9e6244
zergolf1994 <zergolf1994@gmail.com>
2023-09-03 23:33:48 +0700

    fix delete

--- controllers/delete.js
+++ controllers/delete.js
@@ -4,9 +4,12 @@ const path = require("path");
 exports.deleteFile = async (req, res) => {
   try {
     let { file_name } = req.params;
+
     if (!file_name) return res.json({ error: true });
+
     let pathTmp = path.join(global.dirPublic, "upload", file_name);
-    if (!fs.existsSync(pathTmp)) {
+
+    if (fs.existsSync(pathTmp)) {
       fs.removeSync(pathTmp);
       return res.json({ msg: "deleted" });
     } else {
